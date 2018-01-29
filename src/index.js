024e403caf2777c367fb3747575e2aaf21137ce8
Gustavo Sartori Bemfica <gustavo.bemfica@netshoes.com>
2018-01-29 18:01:24 -0200

    constructing redux state

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
 //BrowserRouter => History
 //Route => React Router core, configs stay here
@@ -10,7 +11,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import PostsIndex from './components/posts_index';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
